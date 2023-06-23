f92049cb8ec0ec9c0a6a8b698b9862a23b82f528
zhenghaibiao <547810776@qq.com>
2023-06-24 03:41:22 +0800

    Project Framework

---
+++ Source/ActionRoguelikeGame/ActionRoguelikeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRoguelikeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKEGAME_API AActionRoguelikeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
