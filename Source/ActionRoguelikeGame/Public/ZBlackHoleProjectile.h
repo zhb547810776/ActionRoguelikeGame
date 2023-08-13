daf107cf81ffa8d3acfaae401bce75aeac1b4029
zhenghaibiao <547810776@qq.com>
2023-08-14 03:18:14 +0800

    BlackHole Code Version

---
+++ Source/ActionRoguelikeGame/Public/ZBlackHoleProjectile.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ZProjectile.h"
+#include "ZBlackHoleProjectile.generated.h"
+
+class URadialForceComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKEGAME_API AZBlackHoleProjectile : public AProjectile
+{
+	GENERATED_BODY()
+
+public:
+	AZBlackHoleProjectile();
+	
+protected:
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* RadialForceComp;
+	
+	void Explode(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) __override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
--- Source/ActionRoguelikeGame/Public/ZProjectile.h
+++ Source/ActionRoguelikeGame/Public/ZProjectile.h
@@ -32,7 +32,7 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	UProjectileMovementComponent* MovementComp;
 
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	void Explode(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	
 	// Called when the game starts or when spawned
